abef2dfdf24cc7040d501d4979d103b84a6a6e3c
ahmadsyahrilam <ahmadsyahrilam@gmail.com>
2021-10-01 11:34:40 +0800

    update app.js, index.js, index.html

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
+// component is a piece of a reusable code which can be reused anywhere
+// has own functionality and output
 import './App.css';
+import React from 'react';
+
 
 function App() {
+  //JSX format not HTML
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  
+      <div>Hello para</div>
   );
 }
 
