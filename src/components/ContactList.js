099ec0d1b8daedb96e44c433bd7593a59b2d84a2
ahmadsyahrilam <ahmadsyahrilam@gmail.com>
2021-10-03 14:29:41 +0800

    update contact card & contact list, add user.png images git status

--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -1,5 +1,5 @@
-import { render } from "@testing-library/react";
 import React from "react";
+import ContactCard from "./ContactCard";
 
 // props below is from App.js. --> <ContactList/>
 const ContactList = (props) => {
@@ -10,15 +10,19 @@ const ContactList = (props) => {
     * this function going to take props.contact and map it
     */
     const renderContactList = props.contacts.map((contact) => {
-        return (
-            <div className="item">
-                <div className="content">
-                    <div className="header">{contact.name}</div>
-                    <div>{contact.email}</div>
-                </div>
-                <i className="trash alternate outline icon"></i>
-            </div>
-        )
+        return ( 
+            // <div className="item">
+            //     <div className="content">
+            //         <div className="header">{contact.name}</div>
+            //         <div>{contact.email}</div>
+            //     </div>
+            //     <i className="trash alternate outline icon"></i>
+            // </div>
+
+            //Above code is remove to contactCard and replace with code: <ContactCard></ContactCard>
+
+            <ContactCard contact={contact}></ContactCard>
+        );
     })
 
     return (
